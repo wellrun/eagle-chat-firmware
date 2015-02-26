d214c8a3154a8c0be92872d0e5016bdcd0347d4d
Kevin Ward <antiomiae@gmail.com>
2015-02-26 11:51:03 -0600

    Added skeleton for user_board.h, which will contain common pin and port macros to reference the hardware on the board.

--- config/user_board.h
+++ config/user_board.h
@@ -1,14 +1,34 @@
 /**
  * \file
  *
- * \brief ATxmega192A3U on STK600 board configuration template
+ * \brief EagleChat board configuration. References v0.7 of the PCB design.
  *
- */
- /**
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
 
 #ifndef USER_BOARD_H
 #define USER_BOARD_H
+
+// Pin and port definitions for the hardware on the board
+
+/**
+* SRAM chip
+*/
+
+/**
+* RFM69HW Radio Module
+*/
+
+/**
+* ATSHA crypto module
+*/
+
+/**
+* Break out pins
+*/
+
+/**
+* VBUS sense
+*/
+
 
 #endif // USER_BOARD_H
