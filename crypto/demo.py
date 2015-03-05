d7840e9c901fa2ad9f3bde51481caa638c741f36
Chase Meadors <c.ed.mead@gmail.com>
2015-03-05 07:58:47 -0600

    Cleaned up demo

--- crypto/demo.py
+++ crypto/demo.py
@@ -3,11 +3,13 @@ import binascii as bn
 
 if __name__ == '__main__':
 
+	print "Plug board 'A' in. This will be the 'sender'"
+
 	snum = int(raw_input("Serial port: "))
 
 	ser = serial.Serial(snum - 1);
 
-	print "Asking board to begin encryption process"
+	print "Asking board A to generate keys for the demo and start the encryption process"
 
 	ser.write('e')
 
@@ -18,7 +20,7 @@ if __name__ == '__main__':
 	print "Board said: "
 
 	for line in lines1:
-		print "====> " + line
+		print "====> " + line.strip("\n")
 
 	message = raw_input("Enter a message to tell to the board: ")
 
@@ -31,7 +33,7 @@ if __name__ == '__main__':
 
 	print "Board said: "
 	for line in lines2:
-		print "====> " + line
+		print "====> " + line.strip("\n")
 
 	aPrivate = bn.unhexlify(lines1[1].split(':')[1].strip(" \n"))
 	aPublic = bn.unhexlify(lines1[2].split(':')[1].strip(" \n"))
@@ -45,20 +47,27 @@ if __name__ == '__main__':
 	ser.close();
 
 
-	print "Now, we decrypt on another board. Plug another board in."
+	print "Now plug in board 'B'. This will be the 'receiver'"
 
 	snum = int(raw_input("Serial port: "))
 
 	ser = serial.Serial(snum - 1);
 
-	print "Asking board to begin decryption process"
+	print "Asking board B to begin decryption process"
 
 	ser.write('d')
 
+	print "Telling the board A's public key ..."
 	ser.write(aPublic)
+
+	print "Telling the board it's own private key ..."
 	ser.write(bPrivate)
+
+	print "Telling the board the encrypted message ..."
 	ser.write(encrypted)
 	ser.write("\n")
+
+	print "Telling the board the nonce used to encrypt the message ..."
 	ser.write(nonce)
 
 	response = ser.readline()
