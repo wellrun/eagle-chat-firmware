584352c337cd8d24d28f38cae087985ccd5bf249
Sam Bretz <sam.bretz@okstate.edu>
2015-03-05 03:45:48 -0600

    Radio progress, interrupt added(?), able to read non zero RSSI

--- radio/RFM69.cpp
+++ radio/RFM69.cpp
@@ -32,6 +32,7 @@
 #include "RFM69registers.h"
 
 #include "user_board.h"
+//#include "port_driver.h"
 
 struct spi_device spi_device_conf = {
     .id = IOPORT_CREATE_PIN(PORTE, 4)
@@ -90,10 +91,10 @@ bool RFM69::initialize(uint8_t freqBand, uint8_t nodeID, uint8_t networkID)
 	{
 		/* 0x01 */ { REG_OPMODE, RF_OPMODE_SEQUENCER_ON | RF_OPMODE_LISTEN_OFF | RF_OPMODE_STANDBY },
 		/* 0x02 */ { REG_DATAMODUL, RF_DATAMODUL_DATAMODE_PACKET | RF_DATAMODUL_MODULATIONTYPE_FSK | RF_DATAMODUL_MODULATIONSHAPING_00 }, // no shaping
-		/* 0x03 */ { REG_BITRATEMSB, RF_BITRATEMSB_55555}, // default: 4.8 KBPS
-		/* 0x04 */ { REG_BITRATELSB, RF_BITRATELSB_55555},
-		/* 0x05 */ { REG_FDEVMSB, RF_FDEVMSB_50000}, // default: 5KHz, (FDEV + BitRate / 2 <= 500KHz)
-		/* 0x06 */ { REG_FDEVLSB, RF_FDEVLSB_50000},
+		 /* 0x03 */ { REG_BITRATEMSB, RF_BITRATEMSB_19200}, //default:4.8 KBPS
+    	/* 0x04 */ { REG_BITRATELSB, RF_BITRATELSB_19200},
+    	/* 0x05 */ { REG_FDEVMSB, RF_FDEVMSB_25000}, //default:5khz, (FDEV + BitRate/2 <= 500Khz)
+    	/* 0x06 */ { REG_FDEVLSB, RF_FDEVLSB_25000},
 
 		/* 0x07 */ { REG_FRFMSB, (uint8_t) (freqBand==RF69_315MHZ ? RF_FRFMSB_315 : (freqBand==RF69_433MHZ ? RF_FRFMSB_433 : (freqBand==RF69_868MHZ ? RF_FRFMSB_868 : RF_FRFMSB_915))) },
 		/* 0x08 */ { REG_FRFMID, (uint8_t) (freqBand==RF69_315MHZ ? RF_FRFMID_315 : (freqBand==RF69_433MHZ ? RF_FRFMID_433 : (freqBand==RF69_868MHZ ? RF_FRFMID_868 : RF_FRFMID_915))) },
@@ -108,12 +109,12 @@ bool RFM69::initialize(uint8_t freqBand, uint8_t nodeID, uint8_t networkID)
 		///* 0x13 */ { REG_OCP, RF_OCP_ON | RF_OCP_TRIM_95 }, // over current protection (default is 95mA)
 
 		// RXBW defaults are { REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_24 | RF_RXBW_EXP_5} (RxBw: 10.4KHz)
-		/* 0x19 */ { REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_16 | RF_RXBW_EXP_2 }, // (BitRate < 2 * RxBw)
+		/* 0x19 */ { REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_16 | RF_RXBW_EXP_3 }, // (BitRate < 3 * RxBw)
 		//for BR-19200: /* 0x19 */ { REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_24 | RF_RXBW_EXP_3 },
 		/* 0x25 */ { REG_DIOMAPPING1, RF_DIOMAPPING1_DIO0_01 }, // DIO0 is the only IRQ we're using
 		/* 0x26 */ { REG_DIOMAPPING2, RF_DIOMAPPING2_CLKOUT_OFF }, // DIO5 ClkOut disable for power saving
 		/* 0x28 */ { REG_IRQFLAGS2, RF_IRQFLAGS2_FIFOOVERRUN }, // writing to this bit ensures that the FIFO & status flags are reset
-		/* 0x29 */ { REG_RSSITHRESH, 220 }, // must be set to dBm = (-Sensitivity / 2), default is 0xE4 = 228 so -114dBm
+		/* 0x29 */ { REG_RSSITHRESH, 228 }, // must be set to dBm = (-Sensitivity / 2), default is 0xE4 = 228 so -114dBm
 		///* 0x2D */ { REG_PREAMBLELSB, RF_PREAMBLESIZE_LSB_VALUE } // default 3 preamble bytes 0xAAAAAA
 		/* 0x2E */ { REG_SYNCCONFIG, RF_SYNC_ON | RF_SYNC_FIFOFILL_AUTO | RF_SYNC_SIZE_2 | RF_SYNC_TOL_0 },
 		/* 0x2F */ { REG_SYNCVALUE1, 0x2D },      // attempt to make this compatible with sync1 byte of RFM12B lib
@@ -122,7 +123,7 @@ bool RFM69::initialize(uint8_t freqBand, uint8_t nodeID, uint8_t networkID)
 		/* 0x38 */ { REG_PAYLOADLENGTH, 66 }, // in variable length mode: the max frame size, not used in TX
 		///* 0x39 */ { REG_NODEADRS, nodeID }, // turned off because we're not using address filtering
 		/* 0x3C */ { REG_FIFOTHRESH, RF_FIFOTHRESH_TXSTART_FIFONOTEMPTY | RF_FIFOTHRESH_VALUE }, // TX on FIFO not empty
-		/* 0x3D */ { REG_PACKETCONFIG2, RF_PACKET2_RXRESTARTDELAY_2BITS | RF_PACKET2_AUTORXRESTART_ON | RF_PACKET2_AES_OFF }, // RXRESTARTDELAY must match transmitter PA ramp-down time (bitrate dependent)
+		/* 0x3D */ { REG_PACKETCONFIG2, RF_PACKET2_RXRESTARTDELAY_NONE | RF_PACKET2_AUTORXRESTART_ON | RF_PACKET2_AES_OFF }, //RXRESTARTDELAY must match transmitter PA ramp-down time (bitrate dependent)
 		//for BR-19200: /* 0x3D */ { REG_PACKETCONFIG2, RF_PACKET2_RXRESTARTDELAY_NONE | RF_PACKET2_AUTORXRESTART_ON | RF_PACKET2_AES_OFF }, // RXRESTARTDELAY must match transmitter PA ramp-down time (bitrate dependent)
 		/* 0x6F */ { REG_TESTDAGC, RF_DAGC_IMPROVED_LOWBETA0 }, // run DAGC continuously in RX mode for Fading Margin Improvement, recommended default for AfcLowBetaOn=0
 		{255, 0}
@@ -161,8 +162,23 @@ bool RFM69::initialize(uint8_t freqBand, uint8_t nodeID, uint8_t networkID)
             break;
         }
     }
-    cdc_log_int("ModeReady if no timeout message ", rtc_get_time());
-	//attachInterrupt(_interruptNum, RFM69::isr0, RISING);
+    cdc_log_int("ModeReady if no timeout message ", rtc_get_time());/*
+						//attachInterrupt(_interruptNum, RFM69::isr0, RISING);
+
+					    // Configure PF0 as input, triggered on rising edge.
+						PORT_ConfigurePins( &PORTF, 0x01, false, false, PORT_OPC_TOTEM_gc, PORT_ISC_RISING_gc );
+						// Configure Interrupt0 to have medium interrupt level, triggered by pin 0.
+						PORT_SetPinsAsInput( &PORTF, 0x01 );
+						//ioport_set_pin_sense_mode(_interruptNum, IOPORT_SENSE_RISING);
+						PORT_ConfigureInterrupt0( &PORTF, PORT_INT0LVL_MED_gc, 0x01 );
+						// Enable medium level interrupts in the PMIC.
+						PMIC.CTRL |= PMIC_MEDLVLEN_bm;
+						// Enable the global interrupt flag.
+						sei();*/
+	PORTF.INTCTRL =  (PORTF.INTCTRL & ~PORT_INT0LVL_gm) | PORT_INT0LVL_MED_gc;
+    uint8_t pin_mask = ioport_pin_to_mask(0x01); // Get which pin we are listening on  
+    PORTF.INT0MASK = 0x01; // Set which pin should be source of interrupt
+
 
 	selfPointer = this;
 	_address = nodeID;
@@ -196,32 +212,32 @@ void RFM69::setMode(uint8_t newMode)
 {
 	if (newMode == _mode)
 		return;
-    cdc_log_int("About to set a mode ", rtc_get_time());
+    //cdc_log_int("About to set a mode ", rtc_get_time());
 	switch (newMode) {
 		case RF69_MODE_TX:
-            cdc_log_int("Setting to TX mode ", rtc_get_time());
+            //cdc_log_int("Setting to TX mode ", rtc_get_time());
 			writeReg(REG_OPMODE, (readReg(REG_OPMODE) & 0xE3) | RF_OPMODE_TRANSMITTER);
 			if (_isRFM69HW) setHighPowerRegs(true);
             cdc_log_int("Set to TX mode ", newMode);
 			break;
 		case RF69_MODE_RX:
-            cdc_log_int("Setting to RX mode ", rtc_get_time());
+            //cdc_log_int("Setting to RX mode ", rtc_get_time());
 			writeReg(REG_OPMODE, (readReg(REG_OPMODE) & 0xE3) | RF_OPMODE_RECEIVER);
 			if (_isRFM69HW) setHighPowerRegs(false);
             cdc_log_int("Set to RX mode ", newMode);
 			break;
 		case RF69_MODE_SYNTH:
-            cdc_log_int("Setting to Synth mode ", rtc_get_time());
+            //cdc_log_int("Setting to Synth mode ", rtc_get_time());
 			writeReg(REG_OPMODE, (readReg(REG_OPMODE) & 0xE3) | RF_OPMODE_SYNTHESIZER);
             cdc_log_int("Set to Synth mode ", newMode);
 			break;
 		case RF69_MODE_STANDBY:
-            cdc_log_int("Setting to standby mode ", rtc_get_time());
+            //cdc_log_int("Setting to standby mode ", rtc_get_time());
 			writeReg(REG_OPMODE, (readReg(REG_OPMODE) & 0xE3) | RF_OPMODE_STANDBY);
             cdc_log_int("Set to standby mode ", newMode);
 			break;
 		case RF69_MODE_SLEEP:
-            cdc_log_int("Setting to sleep mode ", rtc_get_time());
+            //cdc_log_int("Setting to sleep mode ", rtc_get_time());
 			writeReg(REG_OPMODE, (readReg(REG_OPMODE) & 0xE3) | RF_OPMODE_SLEEP);
             cdc_log_int("Set to sleep mode ", newMode);
 			break;
@@ -400,7 +416,10 @@ void RFM69::interruptHandler() {
 	//digitalWrite(4, 0);
 }
 
-void RFM69::isr0() { selfPointer->interruptHandler(); }
+//void RFM69::isr0() { selfPointer->interruptHandler(); }
+
+ISR(PORTF_INT0_vect) { RFM69::selfPointer->interruptHandler(); }
+
 
 void RFM69::receiveBegin() {
 	DATALEN = 0;
@@ -419,7 +438,7 @@ void RFM69::receiveBegin() {
 bool RFM69::receiveDone() {
 //ATOMIC_BLOCK(ATOMIC_FORCEON)
 //{
-	//noInterrupts(); // re-enabled in unselect() via setMode() or via receiveBegin()
+	cli();//noInterrupts(); // re-enabled in unselect() via setMode() or via receiveBegin()
 	if (_mode == RF69_MODE_RX && PAYLOADLEN > 0)
 	{
 		setMode(RF69_MODE_STANDBY); // enables interrupts
@@ -427,7 +446,7 @@ bool RFM69::receiveDone() {
 	}
 	else if (_mode == RF69_MODE_RX) // already in RX no payload yet
 	{
-		//interrupts(); // explicitly re-enable interrupts
+		sei();//interrupts(); // explicitly re-enable interrupts
 		return false;
 	}
 	receiveBegin();
--- radio/main.cpp
+++ radio/main.cpp
@@ -37,26 +37,25 @@ extern "C" {
 }
 #include "RFM69.h"
 
-#define NODEID        2    //unique for each node on same network
+
+#define NODEID        101    //unique for each node on same network
 #define NETWORKID     100  //the same on all nodes that talk to each other
 #define GATEWAYID     1
-#define TOID          3
+#define TOID          102
 //Match frequency to the hardware version of the radio on your Moteino (uncomment one):
 //#define FREQUENCY   RF69_433MHZ
 #define FREQUENCY   RF69_868MHZ
 //#define FREQUENCY     RF69_915MHZ
 //#define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
 #define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
-#define ACK_TIME      30 // max # of ms to wait for an ack
-
-#define UDI_CDC_DEFAULT_RATE             9600 
-#define F_CPU                           32000000UL
+#define ACK_TIME      1000 // max # of ms to wait for an ack
+#define NUM_RETRIES   1
 
 int TRANSMITPERIOD = 150; //transmit a packet to gateway so often (in ms)
 char payload[] = "123 ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char buff[20];
 char sendSize=0;
-bool requestACK = false;
+bool requestACK = true;
 RFM69 radio;
 
 int main (void)
@@ -73,22 +72,42 @@ int main (void)
 
 	cdc_start();
     _delay_ms(10);
-    while (udi_cdc_getc() != '2'){
-    cdc_log_int("About to instantiate module ", rtc_get_time());}
+    //while (udi_cdc_getc() != '2');
+    cdc_log_int("About to instantiate module ", rtc_get_time());
     radio = RFM69();
-	while (udi_cdc_getc() != '3'){
-    cdc_log_int("About to intialize module ", rtc_get_time());}
+	//while (udi_cdc_getc() != '3');
+    cdc_log_int("About to intialize module ", rtc_get_time());
     radio.initialize(FREQUENCY,NODEID,NETWORKID);
 	cdc_log_int("Initialized: ", rtc_get_time());
-    //radio.setHighPower();
-/*
-    if(radio.sendWithRetry(TOID, payload, 30, requestACK)){
-        cdc_log_int("Holyshit I think I sent something ", rtc_get_time());
-    } else {
-        cdc_log_int("I didn't send anything because something borked ", rtc_get_time());
-    }
-*/      
+    radio.setHighPower();
+    radio.setPowerLevel(15);
+    radio.promiscuous(true);
+    while(1){
+        //sendWithRetry(uint8_t toAddress, const void* buffer, uint8_t bufferSize, uint8_t retries, uint8_t retryWaitTime) 
+        if(radio.sendWithRetry(TOID, payload, 30, NUM_RETRIES, 30)){
+            cdc_log_int("Holyshit I think I sent something ", rtc_get_time());
+        } else {
+            cdc_log_int("I didn't send anything because something borked ", rtc_get_time());
+        }
 
+        if (radio.receiveDone())
+        {
+            cdc_log_int("Recieved Packet from ", radio.SENDERID);
+            cdc_write_string("Message: ");
+            for (char i = 0; i < radio.DATALEN; i++)
+                udi_cdc_putc((char)radio.DATA[i]);
+            cdc_log_int("RX_RSSI: ", (uint32_t)radio.RSSI);
+
+            if (radio.ACKRequested())
+            {
+                cdc_write_string("ACK requested");
+                radio.sendACK();
+                cdc_write_string(" - ACK sent");
+            }
+        }
+    cdc_log_int("RX_RSSI: ", (uint32_t)radio.RSSI);
+    //cdc_log_int("Temp   : ", (uint32_t)radio.readTemperature());
+    }
 	radio.readAllRegs();
 
     while(1);
