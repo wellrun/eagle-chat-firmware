946e7fdb00ea74b9340336ccb16aa0a5e62d6402
Kevin Ward <antiomiae@gmail.com>
2015-03-11 16:46:50 -0500

    Added thorough stress test to keys demo.

--- keys/main.c
+++ keys/main.c
@@ -21,58 +21,49 @@ int main(void)
 	const key_table_t * table = keys_get_table();
 
 	cdc_log_hex("Loading table: ", keys_load_table());
-	cdc_log_hex_string("Table:\n", (void *)table, PAGE_SIZE);
+	cdc_log_hex_string("Table:\n", (void*)table, PAGE_SIZE);
 
 
 	cdc_log_hex("Resetting table: ", keys_reset_table());
 	keys_load_table();
-	cdc_log_hex_string("Table:\n", (void *)table, PAGE_SIZE);
-
-	uint8_t slot;
-	uint8_t key[PAGE_SIZE];
-	cdc_log_hex_string("Key to store:\n", (void *)key, PAGE_SIZE);
-	cdc_log_hex("Looking for key for id=1. ", keys_has_key(1, &slot));
-
-	cdc_log_hex("Storing key for id=1. ", keys_store_key(1, key));
-	cdc_log_hex_string("Table:\n", (void *)table, PAGE_SIZE);
-
-	cdc_log_hex("Looking for key for id=1. ", keys_has_key(1, &slot));
-	cdc_log_hex("Key slot: ", slot);
-	memset(key, 0xAA, PAGE_SIZE);
-	cdc_log_hex_string("Blanked key:\n", (void *)key, PAGE_SIZE);
-	keys_read_key(slot, key);
-	cdc_log_hex_string("Read key:\n", (void *)key, PAGE_SIZE);
-
-	cdc_write_line("Resetting key.");
-	memset(key, 0xAA, PAGE_SIZE);
-	cdc_log_hex("Storing key for id=1. ", keys_store_key(1, key));
-	cdc_log_hex_string("Table:\n", (void *)table, PAGE_SIZE);
-	memset(key, 0, PAGE_SIZE);
-	keys_read_key(slot, key);
-	cdc_log_hex_string("Read key:\n", (void *)key, PAGE_SIZE);
+	cdc_log_hex_string("Table:\n", (void*)table, PAGE_SIZE);
 
 	cdc_newline(); cdc_newline();
 
-	cdc_log_hex_string("Key to store:\n", (void *)key, PAGE_SIZE);
-	cdc_log_hex("Looking for key for id=2. ", keys_has_key(2, &slot));
+	uint8_t slot;
+	bool has_key;
+	uint8_t key[PAGE_SIZE];
+	for (uint8_t user = 1; user <= MAX_KEY_SLOTS+1; ++user) {
+		slot = 0;
+		has_key = false;
+		memset(key, 0xA, PAGE_SIZE);
+		cdc_log_int("Testing user=", user);
+		has_key = keys_has_key(user, &slot);
+		cdc_log_string("Has key should be false: ", has_key ? "true" : "false");
+		cdc_log_string("Storing key: ", keys_store_key(user, key) == STATUS_OK ? "success" : "failure");
+		has_key = keys_has_key(user, &slot);
+		cdc_log_string("Has key should be true: ", has_key ? "true" : "false");
+		if (has_key) {
+			memset(key, 0x0, PAGE_SIZE);
+			keys_read_key(slot, key);
+			cdc_log_hex_string("Read key:\n", key, PAGE_SIZE);
+			cdc_write_line("Storing new key in old slot.");
+			memset(key, 0xE, PAGE_SIZE);
+			cdc_log_string("Storing key: ", keys_store_key(user, key) == STATUS_OK ? "success" : "failure");
+			has_key = keys_has_key(user, &slot);
+			cdc_log_string("Has key should be true: ", has_key ? "true" : "false");
+			if (has_key) {
+				keys_read_key(slot, key);
+				cdc_log_hex_string("Read key:\n", key, PAGE_SIZE);
+			}
+		}
+		cdc_newline();
+	}
 
-	cdc_log_hex("Storing key for id=2. ", keys_store_key(2, key));
-	cdc_log_hex_string("Table:\n", (void *)table, PAGE_SIZE);
+	cdc_newline();
 
-	cdc_log_hex("Looking for key for id=2. ", keys_has_key(2, &slot));
-	cdc_log_hex("Key slot: ", slot);
-	memset(key, 0xAA, PAGE_SIZE);
-	cdc_log_hex_string("Blanked key:\n", (void *)key, PAGE_SIZE);
-	keys_read_key(slot, key);
-	cdc_log_hex_string("Read key:\n", (void *)key, PAGE_SIZE);
-
-	cdc_write_line("Resetting key.");
-	memset(key, 0xAA, PAGE_SIZE);
-	cdc_log_hex("Storing key for id=2. ", keys_store_key(2, key));
-	cdc_log_hex_string("Table:\n", (void *)table, PAGE_SIZE);
-	memset(key, 0, PAGE_SIZE);
-	keys_read_key(slot, key);
-	cdc_log_hex_string("Read key:\n", (void *)key, PAGE_SIZE);
+	keys_load_table();
+	cdc_log_hex_string("Table:\n", (void*)table, PAGE_SIZE);
 
 	while (1) {
 
