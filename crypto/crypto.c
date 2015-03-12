b26aa295b9f6c4c557d435ca02d462b3f762da02
Chase Meadors <c.ed.mead@gmail.com>
2015-03-11 19:23:44 -0500

    Cleaned up Crypto API to completely mask zero padding concerns. ENCRYPTED_SIZE and DECRYPTED_SIZE now accurately represent the size requirements for outputs of those operations

--- crypto/crypto.c
+++ crypto/crypto.c
@@ -14,17 +14,27 @@ int cr_encrypt(uint8_t *encrypted, uint8_t *message, uint32_t mlen, uint8_t *sPr
 	memset(padded, 0, crypto_box_ZEROBYTES);
 	memcpy(&padded[crypto_box_ZEROBYTES], message, mlen);
 
-	return crypto_box(encrypted, padded, crypto_box_ZEROBYTES + mlen, nonce, rPublic, sPrivate);
+	uint8_t holder[crypto_box_ZEROBYTES + mlen];
+
+	uint8_t ret = crypto_box(holder, padded, crypto_box_ZEROBYTES + mlen, nonce, rPublic, sPrivate);
+
+	memcpy(encrypted, &holder[crypto_box_BOXZEROBYTES], crypto_box_ZEROBYTES + mlen - crypto_box_BOXZEROBYTES);
+
+	return ret;
 }
 
 int cr_decrypt(uint8_t *message, uint8_t *encrypted, uint32_t elen, uint8_t *sPublic, uint8_t *rPrivate, uint8_t *nonce) {
 
-	uint8_t holder[elen];
+	uint8_t holder[elen + crypto_box_BOXZEROBYTES];
 
-	int r = crypto_box_open(holder, encrypted, elen, nonce, sPublic, rPrivate);
+	uint8_t padded[elen + crypto_box_BOXZEROBYTES];
+	memset(padded, 0, crypto_box_BOXZEROBYTES);
+	memcpy(&padded[crypto_box_BOXZEROBYTES], encrypted, elen);
 
-	memcpy(message, &holder[crypto_box_ZEROBYTES], elen - crypto_box_ZEROBYTES);
-	message[elen - crypto_box_ZEROBYTES] = 0;
+	uint8_t r = crypto_box_open(holder, padded, elen + crypto_box_BOXZEROBYTES, nonce, sPublic, rPrivate);
+
+	memcpy(message, &holder[crypto_box_ZEROBYTES], elen + crypto_box_BOXZEROBYTES - crypto_box_ZEROBYTES);
+	message[elen + crypto_box_BOXZEROBYTES - crypto_box_ZEROBYTES] = 0;
 
 	return r;
 
